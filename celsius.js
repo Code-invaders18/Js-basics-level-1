94882ad222a2b0874b31f6ccde2a9cb65c0a8996
Vansh Malik <52025723+Code-invaders18@users.noreply.github.com>
2020-10-06 10:29:36 +0530

    Add files via upload

---
+++ celsius.js
@@ -0,0 +1,5 @@
+var score=100
+score=130
+const bonus=50
+var finalscore=(score+bonus)*1.8
+console.log(finalscore)
---
+++ variables.js
@@ -0,0 +1,8 @@
+let name='vansh'
+let score=100
+let bonus=20
+let total=score + bonus
+console.log(total)
+let firstname='vansh'
+let lastname='malik'
+console.log(firstname+' '+lastname)
